dacb0a61519e455457ec2fb7078d62a43e66bc34
Vadim Litvinenko <133224619+dezlife21@users.noreply.github.com>
2025-05-12 13:16:02 +0300

    Add files via upload

---
+++ LoginApp/LoginApp/LoginControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginApp
+{
+    public partial class LoginControl : UserControl
+    {
+        public LoginControl()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameBox.Text;
+            string password = passwordBox.Password;
+
+            if (!string.IsNullOrWhiteSpace(username) && !string.IsNullOrWhiteSpace(password))
+            {
+                resultText.Text = "Вхід успішний!";
+            }
+            else
+            {
+                resultText.Text = "Будь ласка, заповніть усі поля.";
+            }
+        }
+    }
+}
